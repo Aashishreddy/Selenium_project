2580962d1ded9e7986f85e30f4a8240c16391db5
aashish150 <aashishreds@gmail.com>
2021-06-15 21:12:45 +0530

    Switching Windows
    Back and forth

---
+++ src/test/java/Switching.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Switching {
+
+    @Test
+    public void switchToActiveWindow() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\ASHISH REDDY\\Desktop\\Selenium\\chromedriver_win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/switch-window");
+
+        WebElement buttonElement = driver.findElement(By.id("new-tab-button"));
+        buttonElement.click();
+
+        String originalHandle = driver.getWindowHandle();
+        for(String handle1: driver.getWindowHandles()){
+            driver.switchTo().window(handle1);
+            Thread.sleep(1000);
+
+        }
+        driver.switchTo().window(originalHandle);
+
+        driver.close();
+    }
+}
